1dc6a08665a2fc2ab4790d87f636376cc9a87e10
Marius Jørgensen <git@mjorg.dev>
2025-06-01 14:46:44 +0200

    fix nvm

--- packages/nvm.sh
+++ packages/nvm.sh
@@ -4,10 +4,11 @@ LATEST_VERSION=$(curl -s https://api.github.com/repos/nvm-sh/nvm/releases | \
   jq -r '[.[] | select(.prerelease == false)][0].tag_name')
 
 install() {
-  export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
   execute eval "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$LATEST_VERSION/install.sh | bash"
+
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
   execute nvm install --lts
   execute nvm use --lts
 }
