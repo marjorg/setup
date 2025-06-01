9a1e1f00449769bf603f102e959664f217504ef6
Marius Jørgensen <git@mjorg.dev>
2025-06-01 15:51:16 +0200

    add imagemagick

---
+++ .config/nvim/lua/jorgy/lazy/image.lua
@@ -0,0 +1,8 @@
+return {
+  -- Optional image support in preview window: See `# Preview Mode` for more information
+  "3rd/image.nvim",
+  build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+  opts = {
+    processor = "magick_cli",
+  }
+}
