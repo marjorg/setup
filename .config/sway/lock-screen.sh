44247085510dea7213177aa4fd63714b41983180
Marius Jørgensen <git@mjorg.dev>
2025-06-08 16:03:01 +0200

    lock colors

---
+++ .config/sway/lock-screen.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+swaylock \
+  -i $HOME/.config/sway/bg-2.png \
+  --font "JetBrainsMono Nerd Font" \
+  --font-size 16 \
+  --indicator-radius 130 \
+  --indicator-thickness 7 \
+  --ring-color 7aa2f7 \
+  --key-hl-color 7aa2f7 \
+  --bs-hl-color f7768e \
+  --ring-ver-color 9ece6a \
+  --ring-wrong-color f7768e \
+  --inside-color 1a1b2688 \
+  --inside-ver-color 1a1b2688 \
+  --inside-wrong-color 1a1b2688 \
+  --line-color 00000000 \
+  --line-ver-color 00000000 \
+  --line-wrong-color 00000000 \
+  --separator-color 414868 \
+  --text-color c0caf5 \
+  --text-ver-color c0caf5 \
+  --text-wrong-color c0caf5
