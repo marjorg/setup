85636398448e6d398a5472bddba3b05bed2f3b4e
Marius Jørgensen <git@mjorg.dev>
2025-06-09 22:14:35 +0200

    rename

--- utilities.sh
+++ utilities.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ -d "$DOTFILES_DIR" ]]; then
-  for file in "$DOTFILES_DIR/scripts/"*.sh; do
+  for file in "$DOTFILES_DIR/utilities/"*.sh; do
     if [ -f "$file" ]; then
       source "$file"
     fi
