5882afbd743ef484e2da803a7c6ec4b1d45b344f
Marius Jørgensen <git@mjorg.dev>
2025-06-09 02:30:32 +0200

    mako manual setup

--- packages/sway.sh
+++ packages/sway.sh
@@ -7,6 +7,31 @@ install() {
     install_apt swayidle
 
     # When updating to Ubuntu 23+ this can be replaced by install_apt sway-notification-center
-    install_apt mako-notifier
+    install_apt meson
+    install_apt libwayland-dev
+    install_apt libpango1.0-dev
+    install_apt libcairo2-dev
+    install_apt libsystemd-dev
+    install_apt libgdk-pixbuf2.0-dev
+    install_apt libdbus-1-dev
+
+    execute rm -rf wayland-protocols-1.32.tar.xz wayland-protocols-1.32
+    execute wget https://gitlab.freedesktop.org/wayland/wayland-protocols/-/releases/1.32/downloads/wayland-protocols-1.32.tar.xz
+    execute tar -xf wayland-protocols-1.32.tar.xz
+    execute cd wayland-protocols-1.32
+    execute meson setup build --prefix=/usr --buildtype=release
+    execute ninja -C build
+    execute sudo ninja -C build install
+    execute cd ..
+    execute rm -rf wayland-protocols-1.32.tar.xz wayland-protocols-1.32
+
+    execute rm -rf mako
+    execute git clone https://github.com/emersion/mako.git
+    execute cd mako
+    execute meson build
+    execute ninja -C build
+    execute sudo ninja -C build install
+    execute cd ..
+    execute rm -rf mako
   fi
 }
