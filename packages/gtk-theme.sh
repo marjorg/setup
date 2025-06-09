6629854a0aac922013b513e829778f92429d7bd2
Marius Jørgensen <git@mjorg.dev>
2025-06-09 12:10:36 +0200

    icon theme

--- packages/gtk-theme.sh
+++ packages/gtk-theme.sh
@@ -2,8 +2,12 @@
 
 install() {
   if [[ "$IS_LINUX" == "true" ]]; then
+    install_apt lxappearance # To preview/edit themes/icons
     install_apt sassc
     install_apt gtk2-engines-murrine
     install_apt gnome-themes-extra
+    execute sudo add-apt-repository ppa:papirus/papirus -y
+    execute sudo apt-get update
+    install_apt papirus-icon-theme
   fi
 }
