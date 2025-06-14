933d8154f765e10d58321859be90b87cdba50afd
Marius Jørgensen <git@mjorg.dev>
2025-06-15 00:35:31 +0200

    yay

--- tasks/arch/00_prerequisites.sh
+++ tasks/arch/00_prerequisites.sh
@@ -6,6 +6,13 @@ install() {
     ansible \
     unzip \
     openssh \
-    jq
-  # Note: gnupg2 is provided by gnupg in Arch, so no need to install separately
+    jq \
+    base-devel
+    # Note: gnupg2 is provided by gnupg in Arch, so no need to install separately
+
+    if ! command -v yay &> /dev/null; then
+      git clone https://aur.archlinux.org/yay-bin.git
+      cd yay-bin
+      makepkg -si
+    fi
 }
