91a7b32771815d78df5d8fbee83e1ac1b3946f86
Marius Jørgensen <git@mjorg.dev>
2025-06-14 19:30:13 +0200

    make common

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ elif [[ "$IS_UBUNTU" == true ]]; then
   UBUNTU_VERSION=$(lsb_release -rs)
 
   case "$UBUNTU_VERSION" in
-    22.04)
+    24.04)
       TASKS_DIR="$DOTFILES_DIR/tasks/ubuntu/22.04"
       ;;
     *)
@@ -25,10 +25,48 @@ else
   exit 1
 fi
 
+if [[ -f "$TASKS_DIR/00_prerequisites.sh" ]]; then
+  source "$TASKS_DIR/00_prerequisites.sh"
+
+  if declare -F install > /dev/null; then
+    install
+    unset -f install
+  fi
+fi
+
+if [[ "$WORK" == false ]]; then
+  if [[ ! -f "$DOTFILES_DIR/modules/private/vault.yml" ]]; then
+    echo "🚨 Private submodule required"
+    echo "Generate SSH and GPG keys and add them to GitHub"
+    echo "Then run: 'git submodule update --init --recursive'"
+    exit 1
+  fi
+
+  read -sp "Vault password: " VAULT_PW
+  echo
+
+  VAULT_PW_FILE=$(mktemp)
+  chmod 600 "$VAULT_PW_FILE"
+  echo "$VAULT_PW" > "$VAULT_PW_FILE"
+
+  ansible-playbook "$DOTFILES_DIR/main.yml" \
+    -e "work_mode=$WORK" \
+    -e "email=$EMAIL" \
+    --vault-password-file "$VAULT_PW_FILE"
+else
+  ansible-playbook "$DOTFILES_DIR/main.yml" \
+    -e "work_mode=$WORK" \
+    -e "email=$EMAIL"
+fi
+exit 0
 for task in "$TASKS_DIR"/*.sh; do
   if [[ -f "$task" ]]; then
     source "$task"
     TASK_NAME=$(basename "${task%.sh}")
+
+    if TASK_NAME == "00_prerequisites"; then
+      continue
+    fi
 
     if declare -F install > /dev/null; then
       debug "Running $TASK_NAME install..."
@@ -37,3 +75,12 @@ for task in "$TASKS_DIR"/*.sh; do
     fi
   fi
 done
+
+if [[ "$IS_LINUX" == true ]]; then
+  if [[ "$SHELL" != "/usr/bin/zsh" ]]; then
+    execute chsh -s $(which zsh)
+    log "Changed default shell to zsh, restart terminal"
+  else
+    debug "Default shell is already zsh"
+  fi
+fi
