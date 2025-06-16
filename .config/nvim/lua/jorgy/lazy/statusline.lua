4b948603028dac8844f530ed67f7fb0d31c6760c
Marius Jørgensen <git@mjorg.dev>
2025-06-16 22:29:28 +0200

    statusline

---
+++ .config/nvim/lua/jorgy/lazy/statusline.lua
@@ -0,0 +1,22 @@
+-- https://github.com/nvim-lualine/lualine.nvim
+
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "tokyonight",
+        icons_enabled = true,
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = { "filetype" },
+        lualine_y = {},
+        lualine_z = { "location" },
+      },
+    })
+  end,
+}
