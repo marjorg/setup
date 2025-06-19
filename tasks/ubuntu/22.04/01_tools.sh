690a6e43493de7b52e29a41be15ab83357b2ad89
Marius Jørgensen <git@mjorg.dev>
2025-06-19 17:44:41 +0200

    remove unused stuff

--- tasks/ubuntu/22.04/01_tools.sh
+++ tasks/ubuntu/22.04/01_tools.sh
@@ -13,9 +13,7 @@ install() {
     ripgrep \
     tmux \
     zsh \
-    ghostty \
     zoxide
-
 }
 
 update() {
--- tasks/ubuntu/22.04/terminal.sh
+++ tasks/ubuntu/22.04/terminal.sh
@@ -6,14 +6,6 @@ install() {
   curl -fsSL https://apt.cli.rs/pubkey.asc | sudo tee -a /usr/share/keyrings/rust-tools.asc
   curl -fsSL https://apt.cli.rs/rust-tools.list | sudo tee /etc/apt/sources.list.d/rust-tools.list
 
-  if [ ! -d "${TPM}" ]; then
-    git clone https://github.com/tmux-plugins/tpm "${TPM}"
-  else
-    cd "${TPM}"
-    git pull --porcelain
-    cd -
-  fi
-
   curl -s https://ohmyposh.dev/install.sh | bash -s
 }
 
