de509e4bea8db8fdbd7ab806e9d2793f70f8d35d
Marius Jørgensen <git@mjorg.dev>
2025-06-25 22:16:48 +0200

    remove

--- tasks/ubuntu/24.10/terminal.sh
+++ tasks/ubuntu/24.10/terminal.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-TPM="${HOME}/.tmux/plugins/tpm"
-
 install() {
   curl -fsSL https://apt.cli.rs/pubkey.asc | sudo tee -a /usr/share/keyrings/rust-tools.asc
   curl -fsSL https://apt.cli.rs/rust-tools.list | sudo tee /etc/apt/sources.list.d/rust-tools.list
