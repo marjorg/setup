9155acc4ca033ac1ca59593a1aa5f8f818ca48ca
Marius Jørgensen <git@mjorg.dev>
2025-07-07 20:25:53 +0200

    drivers+gaming

--- arch/installs/drivers.sh
+++ arch/installs/drivers.sh
@@ -2,4 +2,9 @@
 
 PACMAN_PACKAGES+=(
   ntfs-3g # Full NTFS read/write support on drives shared with Windows
+  nvidia-settings # For GPU tweaking
+  lib32-nvidia-utils # For 32-bit game compatibility
+  vulkan-tools # Vulkan supprot
+  gamemode # Optimise Linux system performance on demand, requires setting launch options in Steam `gamemoderun %command%`
+  mangohud # Overlay for monitoring FPS++, requires setting launch options in Steam `mangohud %command%`
 )
