6819c88be23392808aeca5697965c1991c451222
Marius Jørgensen <git@mjorg.dev>
2025-07-07 23:23:38 +0200

    nvm

--- arch/installs/javascript.sh
+++ arch/installs/javascript.sh
@@ -4,4 +4,16 @@ post_install() {
   if ! command -v bun &> /dev/null; then
     curl -fsSL https://bun.sh/install | bash
   fi
+
+  if [ ! -f "$HOME/.config/nvm/nvm.sh" ]; then
+    curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+  fi
+
+  # Make available in current script
+  export NVM_DIR="$HOME/.config/nvm"
+  source "$NVM_DIR/nvm.sh"
+
+  if ! nvm ls --no-alias | grep -q '\->'; then
+    nvm install --lts
+  fi
 }
