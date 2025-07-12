224343e075939874d046df39945938022181c39d
Marius Jørgensen <git@mjorg.dev>
2025-07-12 22:42:42 +0200

    update

--- update.sh
+++ update.sh
@@ -6,6 +6,7 @@ if [[ "$IS_UBUNTU" == true ]]; then
   execute sudo apt-get update
   execute sudo apt-get upgrade -y
 elif [[ "$IS_ARCH" == true ]]; then
+  execute sudo pacman -S archlinux-keyring --noconfirm
   execute sudo pacman -Syu --noconfirm
 fi
 
@@ -13,5 +14,6 @@ if [[ "$IS_UBUNTU" == true ]]; then
   execute sudo apt-get autoremove
   execute sudo apt-get autoclean
 elif [[ "$IS_ARCH" == true ]]; then
+  execute sudo pacman -Rns --noconfirm $(pacman -Qtdq 2>/dev/null)
   execute sudo pacman -Sc --noconfirm
 fi
