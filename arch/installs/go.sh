856e7990e6284a7e05ec32956b8eac96cefb0575
Marius Jørgensen <git@mjorg.dev>
2025-07-24 21:20:03 +0200

    fix golangci-lint

--- arch/installs/go.sh
+++ arch/installs/go.sh
@@ -4,12 +4,17 @@ PACMAN_PACKAGES+=(
   go
 )
 
+YAY_PACKAGES+=(
+  golangci-lint
+)
+
 post_install() {
   go install golang.org/x/tools/gopls@latest
   go install github.com/go-delve/delve/cmd/dlv@latest
-  go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+  go install github.com/nametake/golangci-lint-langserver@latest
   go install github.com/air-verse/air@latest
   go install github.com/sqlc-dev/sqlc/cmd/sqlc@latest
+  go install github.com/pressly/goose/v3/cmd/goose@latest
 }
 
 # golangci-lint run
