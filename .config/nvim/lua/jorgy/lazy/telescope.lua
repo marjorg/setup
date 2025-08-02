9e01af4defa9de65fc09c2eaf3354a8b209b7d0b
Marius Jørgensen <git@mjorg.dev>
2025-08-02 11:27:20 +0200

    search hidden

--- .config/nvim/lua/jorgy/lazy/telescope.lua
+++ .config/nvim/lua/jorgy/lazy/telescope.lua
@@ -8,13 +8,33 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
 
   config = function()
-    require('telescope').setup{}
+    require('telescope').setup {}
 
     local builtin = require('telescope.builtin')
 
-    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+    vim.keymap.set('n', '<leader>s?', builtin.help_tags, { desc = '[S]earch [?]Help' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
     vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>sh', function()
+      builtin.find_files({
+        hidden = true,
+        file_ignore_patterns = {
+          "%.git/.*",
+          "node_modules/.*",
+          "%.git/.*",
+          "%.DS_Store",
+          "dist/.*",
+          "build/.*",
+          "%.cache/.*",
+          "vendor/.*",
+          "%.bash_history",
+          "%.zsh_history",
+          "%.npm/.*",
+          "%.yarn/.*",
+          "%.env.*"
+        }
+      })
+    end, { desc = '[S]earch [H]idden files' })
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
