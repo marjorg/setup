6d5a050754bb903182a7989283562794f9900622
Marius Jørgensen <git@mjorg.dev>
2025-10-29 22:00:25 +0100

    add mise support

--- arch/install.sh
+++ arch/install.sh
@@ -7,6 +7,7 @@ source "$SCRIPT_DIR/../utils.sh"
 
 PACMAN_PACKAGES=()
 YAY_PACKAGES=()
+MISE_PACKAGES=()
 PRE_INSTALL_SCRIPTS=()
 POST_INSTALL_SCRIPTS=()
 
@@ -40,27 +41,53 @@ else
   debug "No pre-install scripts to run."
 fi
 
-# Deduplicate
 PACMAN_PACKAGES=($(printf "%s\n" "${PACMAN_PACKAGES[@]}" | sort -u))
-YAY_PACKAGES=($(printf "%s\n" "${YAY_PACKAGES[@]}" | sort -u))
+PACMAN_PACKAGES_NOT_INSTALLED=()
 
-if [ "${#PACMAN_PACKAGES[@]}" -gt 0 ]; then
-  log "Installing Pacman packages: ${PACMAN_PACKAGES[*]}"
-  execute sudo pacman -Sy --noconfirm --needed "${PACMAN_PACKAGES[@]}" >>"$LOG_FILE" 2>&1 || log "Pacman installation failed."
-else
-  debug "No Pacman packages to install."
-fi
-
-if [ "${#YAY_PACKAGES[@]}" -gt 0 ]; then
-  if ! command -v yay &>/dev/null; then
-    log "Yay is not installed. Skipping AUR package installation."
+for pkg in "${PACMAN_PACKAGES[@]}"; do
+  if ! pacman -Q "$pkg" &>/dev/null; then
+    PACMAN_PACKAGES_NOT_INSTALLED+=("$pkg")
   else
-    log "Installing Yay packages: ${YAY_PACKAGES[*]}"
-    execute yay -Sy --noconfirm --needed "${YAY_PACKAGES[@]}" >>"$LOG_FILE" 2>&1 || log "Yay installation failed."
+    debug "Package '$pkg' is already installed. Skipping."
   fi
+done
+
+if [ "${#PACMAN_PACKAGES_NOT_INSTALLED[@]}" -gt 0 ]; then
+  log "Installing Pacman packages: ${PACMAN_PACKAGES_NOT_INSTALLED[*]}"
+  execute sudo pacman -Sy --noconfirm --needed "${PACMAN_PACKAGES_NOT_INSTALLED[@]}" >>"$LOG_FILE" 2>&1 || log "Pacman installation failed."
 else
-  debug "No Yay packages to install."
+  log "No Pacman packages to install."
 fi
+
+YAY_PACKAGES=($(printf "%s\n" "${YAY_PACKAGES[@]}" | sort -u))
+YAY_PACKAGES_NOT_INSTALLED=()
+
+for pkg in "${YAY_PACKAGES[@]}"; do
+  if ! yay -Q "$pkg" &>/dev/null; then
+    YAY_PACKAGES_NOT_INSTALLED+=("$pkg")
+  else
+    debug "Yay package '$pkg' is already installed. Skipping."
+  fi
+done
+
+if [ "${#YAY_PACKAGES_NOT_INSTALLED[@]}" -gt 0 ]; then
+  log "Installing Yay packages: ${YAY_PACKAGES_NOT_INSTALLED[*]}"
+  execute yay -Sy --noconfirm --needed "${YAY_PACKAGES_NOT_INSTALLED[@]}" >>"$LOG_FILE" 2>&1 || log "Yay installation failed."
+else
+  log "No Yay packages to install."
+fi
+
+MISE_PACKAGES=($(printf "%s\n" "${MISE_PACKAGES[@]}" | sort -u))
+# TODO: Add already installed filter?
+
+if [ "${#MISE_PACKAGES[@]}" -gt 0 ]; then
+  log "Installing Mise packages: ${MISE_PACKAGES[*]}"
+  execute mise use --global "${MISE_PACKAGES[@]}" >>"$LOG_FILE" 2>&1 || log "Mise installation failed."
+else
+  log "No Mise packages to install."
+fi
+
+# TODO: Might have to source shell here to make go install work after mise install
 
 if [ "${#POST_INSTALL_SCRIPTS[@]}" -gt 0 ]; then
   log "Running post-install scripts..."
