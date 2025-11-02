d6e1be31e41148242ed0df8f50ea14c1a514fe10
Marius Jørgensen <git@mjorg.dev>
2025-11-02 13:14:56 +0100

    tpm

--- arch/installs/terminal.sh
+++ arch/installs/terminal.sh
@@ -11,3 +11,13 @@ YAY_PACKAGES+=(
 )
 
 # TODO: Post: Update default terminal in omarchy settings, see https://github.com/basecamp/omarchy/blob/e58569b947012eed458488e8c7777c5da27f7a9c/bin/omarchy-install-terminal
+
+post_install() {
+  TPM_DIR="$HOME/.tmux/plugins/tpm"
+
+  if [[ ! -d "$TPM_DIR" ]]; then
+    git clone --quiet https://github.com/tmux-plugins/tpm "$TPM_DIR"
+  else
+    git -C "$TPM_DIR" pull --quiet
+  fi
+}
